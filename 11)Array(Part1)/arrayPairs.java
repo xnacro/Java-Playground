76177f423b241cf4d315ef7cf71751e12498a469
xnacro <businessofficialtech@gmail.com>
2026-09-19 17:35:31 +0530

    feat(array): count and print total pairs in arrayPairs

--- 11)Array(Part1)/arrayPairs.java
+++ 11)Array(Part1)/arrayPairs.java
@@ -1,13 +1,15 @@
 public class arrayPairs {
 
     public static void printPair(int arr[]) {
-        
+        int totalPairs = 0;
         for(int i = 0; i< arr.length; i++) {
            for(int j = i+1 ; j< arr.length; j++) {
                System.out.println("(" + arr[i] +"," + arr[j] + ")");
+               totalPairs++;
            }
-           System.out.println();
+           System.out.println("");
        }
+       System.out.println(totalPairs);
     }
     public static void main(String[] args) {
        int arr[] = {2,4,6,8,10};
